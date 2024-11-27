edaf1c1f9a28f8759c2553a2cdcdb64fc3792d82
Hadnan Basilio <basiliohadnan@gmail.com>
2024-11-27 10:38:29 -0300

    add ts example test file

---
+++ tests/firsttest.ts
@@ -0,0 +1,22 @@
+import {chromium} from '@playwright/test';
+
+//Async block
+(async() => {
+    //Creating the browser instance
+    const browser = await chromium.launch({
+        headless: false
+
+    });
+
+    //Creating the browser context
+    const browserContext = await browser.newContext();
+
+    //Create the page
+    const page = await browserContext.newPage();
+
+    //Navigate to google.com
+    await page.goto("https://google.com");
+
+    //Close browser
+    await browser.close();
+})();
